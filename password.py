66f5701fd9aa878b78e8e07f97ae6ecdd2cced5d
worksofgenius <54582562+worksofgenius@users.noreply.github.com>
2023-05-09 22:59:34 +0900

    Upload password.py

---
+++ password.py
@@ -0,0 +1,38 @@
+print("Please, come forward.")
+
+print()
+
+while True:
+  print('May I have the password, please?')
+  
+  password = input()
+  
+  if password != 'Fidelio':
+    
+    continue
+
+  print("That's right, sir! \n \nThat is the password... for admittance. \n \nBut may I ask, what is the password for the house?")
+  
+  house_password = input()
+  
+  if house_password == '...':
+
+    break
+
+print("""
+That's unfortunate! 
+
+Because here, it doesn't matter whether you have forgotten it... 
+
+Or if you never knew it. 
+
+You will kindly remove your mask.
+
+Now, get undressed.
+
+Remove your clothes.
+
+REMOVE YOUR CLOTHES!
+
+Or would you like us to do it for you?
+""")
